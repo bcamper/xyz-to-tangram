96cb426ea01ce5a5b6a10eea2ccd0786179be0dd
Brett Camper <bcamper@alum.mit.edu>
2019-05-10 15:30:11 -0400

    move library file to lib folder

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    input: 'index.js',
+    input: 'lib/index.js',
     output: {
         file: 'dist/xyz-to-tangram.js',
         format: 'umd',
